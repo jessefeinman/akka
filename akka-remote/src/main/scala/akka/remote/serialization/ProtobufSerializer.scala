0ac5b742d39825d9094ce8e247fb518dcd026b02
Feinman, Jesse <Jesse.Feinman@gmail.com>
2018-05-14 20:28:50 -0400

    rec call on atomic reference replaced with concurrent triemap to make code cleaner and more maintainable.

--- akka-remote/src/main/scala/akka/remote/serialization/ProtobufSerializer.scala
+++ akka-remote/src/main/scala/akka/remote/serialization/ProtobufSerializer.scala
@@ -5,13 +5,12 @@
 package akka.remote.serialization
 
 import java.lang.reflect.Method
-import java.util.concurrent.atomic.AtomicReference
 
 import akka.actor.{ ActorRef, ExtendedActorSystem }
 import akka.remote.WireFormats.ActorRefData
 import akka.serialization.{ Serialization, BaseSerializer }
 
-import scala.annotation.tailrec
+import scala.collection.concurrent
 
 object ProtobufSerializer {
   private val ARRAY_OF_BYTE_ARRAY = Array[Class[_]](classOf[Array[Byte]])
@@ -40,52 +39,21 @@ object ProtobufSerializer {
  */
 class ProtobufSerializer(val system: ExtendedActorSystem) extends BaseSerializer {
 
-  private val parsingMethodBindingRef = new AtomicReference[Map[Class[_], Method]](Map.empty)
-  private val toByteArrayMethodBindingRef = new AtomicReference[Map[Class[_], Method]](Map.empty)
+  private val parsingMethodBindingRef = concurrent.TrieMap.empty[Class[_], Method]
+  private val toByteArrayMethodBindingRef = concurrent.TrieMap.empty[Class[_], Method]
 
   override def includeManifest: Boolean = true
 
-  override def fromBinary(bytes: Array[Byte], manifest: Option[Class[_]]): AnyRef = {
-    manifest match {
-      case Some(clazz) ⇒
-        @tailrec
-        def parsingMethod(method: Method = null): Method = {
-          val parsingMethodBinding = parsingMethodBindingRef.get()
-          parsingMethodBinding.get(clazz) match {
-            case Some(cachedParsingMethod) ⇒ cachedParsingMethod
-            case None ⇒
-              val unCachedParsingMethod =
-                if (method eq null) clazz.getDeclaredMethod("parseFrom", ProtobufSerializer.ARRAY_OF_BYTE_ARRAY: _*)
-                else method
-              if (parsingMethodBindingRef.compareAndSet(parsingMethodBinding, parsingMethodBinding.updated(clazz, unCachedParsingMethod)))
-                unCachedParsingMethod
-              else
-                parsingMethod(unCachedParsingMethod)
-          }
-        }
-        parsingMethod().invoke(null, bytes)
-
-      case None ⇒ throw new IllegalArgumentException("Need a protobuf message class to be able to serialize bytes using protobuf")
-    }
+  override def fromBinary(bytes: Array[Byte], manifest: Option[Class[_]]): AnyRef = manifest match {
+    case Some(clazz) ⇒
+      parsingMethodBindingRef
+        .getOrElseUpdate(clazz, clazz.getDeclaredMethod("parseFrom", ProtobufSerializer.ARRAY_OF_BYTE_ARRAY: _*))
+        .invoke(null, bytes)
+    case None ⇒ throw new IllegalArgumentException("Need a protobuf message class to be able to serialize bytes using protobuf")
   }
 
-  override def toBinary(obj: AnyRef): Array[Byte] = {
-    val clazz = obj.getClass
-    @tailrec
-    def toByteArrayMethod(method: Method = null): Method = {
-      val toByteArrayMethodBinding = toByteArrayMethodBindingRef.get()
-      toByteArrayMethodBinding.get(clazz) match {
-        case Some(cachedtoByteArrayMethod) ⇒ cachedtoByteArrayMethod
-        case None ⇒
-          val unCachedtoByteArrayMethod =
-            if (method eq null) clazz.getMethod("toByteArray")
-            else method
-          if (toByteArrayMethodBindingRef.compareAndSet(toByteArrayMethodBinding, toByteArrayMethodBinding.updated(clazz, unCachedtoByteArrayMethod)))
-            unCachedtoByteArrayMethod
-          else
-            toByteArrayMethod(unCachedtoByteArrayMethod)
-      }
-    }
-    toByteArrayMethod().invoke(obj).asInstanceOf[Array[Byte]]
-  }
+  override def toBinary(obj: AnyRef): Array[Byte] =
+    toByteArrayMethodBindingRef
+      .getOrElseUpdate(obj.getClass, obj.getClass.getMethod("toByteArray"))
+      .invoke(obj).asInstanceOf[Array[Byte]]
 }
